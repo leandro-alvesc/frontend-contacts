21ade180e86aac825585b60cdb7ad5721d6747f6
Leandro Alves <leandrofelipecunha@hotmail.com>
2022-01-31 10:08:07 -0300

    feat: create login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="login">
     <h1>Login</h1>
-    <input type="text" placeholder="Email...">
-    <input type="password" placeholder="Senha...">
-    
+    <input type="text" placeholder="Email..." />
+    <input type="password" placeholder="Senha..." />
+    <div>
+      <v-btn>Login</v-btn>
+    </div>
+    <p><a href="">Cadastre-se</a></p>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
