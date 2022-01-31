dcc59b6f547537cedbc4639f1314773d28961c69
Leandro Alves <leandrofelipecunha@hotmail.com>
2022-01-31 10:04:13 -0300

    feat: install vue

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <input type="text" placeholder="Email...">
+    <input type="password" placeholder="Senha...">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
